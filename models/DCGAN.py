d1689893a5430d83a90c38db82febed8dafb3dc5
Chienping Tsung <jianpingzong@outlook.com>
2021-01-14 18:38:55 +0800

    Create DCGAN.py

---
+++ models/DCGAN.py
@@ -0,0 +1,84 @@
+from torch import nn
+
+
+class Generator(nn.Module):
+    def __init__(self):
+        super(Generator, self).__init__()
+        self.main = nn.Sequential(
+            # (N, 100, 1, 1)
+            nn.ConvTranspose2d(100, 1024, 4, 1, 0, bias=False),
+            nn.BatchNorm2d(1024),
+            nn.ReLU(True),
+            # (N, 1024, 4, 4)
+            nn.ConvTranspose2d(1024, 512, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(512),
+            nn.ReLU(True),
+            # (N, 512, 8, 8)
+            nn.ConvTranspose2d(512, 256, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.ReLU(True),
+            # (N, 256, 16, 16)
+            nn.ConvTranspose2d(256, 128, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(128),
+            nn.ReLU(True),
+            # (N, 128, 32, 32)
+            nn.ConvTranspose2d(128, 64, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(True),
+            # (N, 64, 64, 64)
+            nn.ConvTranspose2d(64, 32, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(32),
+            nn.ReLU(True),
+            # (N, 32, 128, 128)
+            nn.ConvTranspose2d(32, 16, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(16),
+            nn.ReLU(True),
+            # (N, 16, 256, 256)
+            nn.ConvTranspose2d(16, 3, 4, 2, 1, bias=False),
+            nn.Tanh()
+            # (N, 3, 512, 512)
+        )
+
+    def forward(self, input):
+        return self.main(input)
+
+
+class Discriminator(nn.Module):
+    def __init__(self):
+        super(Discriminator, self).__init__()
+        self.main = nn.Sequential(
+            # (N, 3, 512, 512)
+            nn.Conv2d(3, 16, 4, 2, 1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            # (N, 16, 256, 256)
+            nn.Conv2d(16, 32, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(32),
+            nn.LeakyReLU(0.2, inplace=True),
+            # (N, 32, 128, 128)
+            nn.Conv2d(32, 64, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.LeakyReLU(0.2, inplace=True),
+            # (N, 64, 64, 64)
+            nn.Conv2d(64, 128, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(0.2, inplace=True),
+            # (N, 128, 32, 32)
+            nn.Conv2d(128, 256, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.2, inplace=True),
+            # (N, 256, 16, 16)
+            nn.Conv2d(256, 512, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.2, inplace=True),
+            # (N, 512, 8, 8)
+            nn.Conv2d(512, 1024, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(1024),
+            nn.LeakyReLU(0.2, inplace=True),
+            # (N, 1024, 4, 4)
+            nn.Conv2d(1024, 1, 4, 1, 0),
+            nn.Sigmoid()
+            # (N, 1, 1, 1)
+        )
+
+    def forward(self, input):
+        return self.main(input)
